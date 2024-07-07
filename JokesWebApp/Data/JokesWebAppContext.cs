7f9d6563acb1afd9ed5c22dcbbdd48579bad7937
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-07-07 22:55:33 +0800

    finished a crash course and create a simple app
    next will be advance topics

---
+++ JokesWebApp/Data/JokesWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApp.Models;
+
+namespace JokesWebApp.Data
+{
+    public class JokesWebAppContext : DbContext
+    {
+        public JokesWebAppContext (DbContextOptions<JokesWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokesWebApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
